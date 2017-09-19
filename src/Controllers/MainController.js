c2ee342e0939b5f3c7dcbd075cdb3cc3d1b7bcbd
Jesus Vzz <jesus.elef@gmail.com>
2017-09-19 11:10:34 -0500

    agregando mas funciones de angularjs

--- src/Controllers/MainController.js
+++ src/Controllers/MainController.js
@@ -1,21 +1,41 @@
 angular.module('moduleApp', [])
-.controller('Control', function ($scope) {
+.controller('Control', function ($scope,$http) {
+
+    $scope.posts = [];
+    $scope.nuevoPost = {};
+
+    $http.get("http://jsonplaceholder.typicode.com/posts")
+          .then(function(datos){
+            $scope.posts = datos.data;
+           }, function(error){
+           });
+
+    $scope.agergaPost = function () {
+    $http.post("http://jsonplaceholder.typicode.com/posts",
+          {title : $scope.nuevoPost.title,
+           body : $scope.nuevoPost.body,
+           userId : 1})
+          .then(function(response){
+            $scope.posts.push($scope.nuevoPost);
+            $scope.agregarPost = {};
+            console.log("ya termino");
+          })};
+
     $scope.libros = [
         {'titulo': 'Alimentos y Bebidas', 'autor': 'Mexico',licencias : 10},
         {'titulo': 'Farmaceutica', 'autor': 'Mexico, Brazil',licencias : 5},
         {'titulo': 'Cuidado personal ', 'autor': 'Mexico, Guatemala, Peru, Canada', licencias : 15}
     ];
 
+    $scope.nuevoComentario = {};
     $scope.comentarios = [
-        {estrellas : 5,texto : "Me encanta" , usuario : "jesus.elef@gmail.com", seccion : "Pagina"},
-        {estrellas : 3,texto : "Parece bien" , usuario : "usuarioNoRegistrado", seccion :"Sineti"}
+        {estrellas : 5,texto : "Me encanta" , usuario : "jesus.elef@gmail.com"},
+        {estrellas : 3,texto : "Parece bien" , usuario : "usuarioNoRegistrado"}
     ];
-
     $scope.agregarComentario = function() {
-    $scope.comentarios.push({estrellas : estrellasView, texto : textoView ,usuario : "usuarioView", seccion: "Sineti"});
-    $scope.usuarioView = '';
-    $scope.textoView = '';
-    };
+      $scope.comentarios.push($scope.nuevoComentario);
+      $scope.nuevoComentario = {};
+};
 
 
 })
